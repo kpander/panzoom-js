56a2fa789c2a1ab41f74e2d22e820c795d673e29
Kendall Anderson <kpander@invisiblethreads.com>
2023-08-13 08:08:58 -0400

    Adds initial PanZoom and PanZoomDragUI classes
    These are not yet complete. These are the current prototype states
    from testing elsewhere. Still need to add the ability to call other
    code when events change (specifically in the Drag class) so we can
    trigger other UI items as needed.
    
    These are functional, but we haven't started to consider developer
    experience yet.

---
+++ src/PanZoom.UI.Drag.js
@@ -0,0 +1,153 @@
+"use strict";
+/**
+ * @file
+ * PanZoom.UI.Drag.js
+ *
+ * Add drag and drop panning functionality (and double click zoom) to a PanZoom
+ * object instance.
+ *
+ * Click-and-drag to pan.
+ * Double-click to zoom in.
+ * Double-click + SHIFT key to zoom out.
+ *
+ * Works for both mouse and touch inputs.
+ */
+
+class PanZoomDragUI {
+
+  /**
+   * @param object els with keys:
+   *   drag: element we add mouse events to for dragging
+   *   transition: element we block transitions on when dragging
+   *   scroll: element we block scroll on when dragging
+   * @param object pzInstance PanZoom object instance
+   */
+  constructor(els = {}, pzInstance) {
+    this.els = els;
+    this.pz = pzInstance;
+
+    this.state = {
+      isDragging: false,
+      startX: 0,
+      startY: 0,
+      deltaX: 0,
+      deltaY: 0,
+      tapCount: 0,
+      tapTimer: null,
+    };
+    this.config = {
+      doubleTapThreshold: 300,
+    };
+
+    this._addBindings();
+  }
+
+  /**
+   * Add event listeners for both mouse and touch events.
+   */
+  _addBindings() {
+    this.els.drag.addEventListener("mousedown", this._handleMouseDown.bind(this));
+    this.els.drag.addEventListener("touchstart", this._handleTouchDown.bind(this));
+
+    this.els.drag.addEventListener("mousemove", this._handleMouseMove.bind(this));
+    this.els.drag.addEventListener("touchmove", this._handleMouseMove.bind(this));
+
+    this.els.drag.addEventListener("mouseup", this._handleMouseUp.bind(this));
+    this.els.drag.addEventListener("touchend", this._handleMouseUp.bind(this));
+  }
+
+  _handleTouchDown(/*event*/) {
+    this.state.tapCount++;
+
+    if (this.state.tapCount === 1) {
+      this.state.tapTimer = setTimeout(function() {
+        this.state.tapCount = 0;
+      }, this.config.doubleTapThreshold);
+    } else if (this.state.tapCount === 2) {
+      // This is a double-tap event.
+      clearTimeout(this.state.tapTimer);
+      this.state.tapCount = 0;
+
+      /*updateSlider(*/this.pz.zoomIn()/*.zoom)*/; // @todo
+    }
+
+    this._initDrag();
+  }
+
+  _handleMouseDown(/*event*/) {
+    this._initDrag();
+  }
+
+  _handleMouseMove(event) {
+    if (!this.state.isDragging) return;
+
+    const currentX = event.clientX || event.touches[0].clientX;
+    const currentY = event.clientY || event.touches[0].clientY;
+
+    this.state.deltaX = currentX - this.state.startX;
+    this.state.deltaY = currentY - this.state.startY;
+
+    const dragOffset = {
+      x: this.state.deltaX,
+      y: this.state.deltaY,
+    };
+    this.state.dragTarget = this.pz.panOffsetLive(dragOffset);
+  }
+
+  _handleMouseUp() {
+    if (this.state.isDragging) {
+      this._resetDrag();
+    }
+  }
+
+  _initDrag() {
+    this._disableWindowScroll();
+    this._disableTransitions();
+
+    this.state.isDragging = true;
+    this.state.startX = event.clientX || event.touches[0].clientX;
+    this.state.startY = event.clientY || event.touches[0].clientY;
+  }
+
+  _resetDrag() {
+    this._enableWindowScroll();
+    this._enableTransitions();
+
+    this.state.isDragging = false;
+    if (this.state.dragTarget) {
+      this.pz.panTo(this.state.dragTarget);
+      this.state.dragTarget = null;
+    }
+  }
+
+  _disableWindowScroll() {
+    if (this.els.scroll) {
+      this.els.scroll.style.overscrollBehavior = "contain";
+      this.els.scroll.style.overscrollBehaviorY = "contain";
+      this.els.scroll.style.overflowX = "hidden";
+      this.els.scroll.style.overflowY = "hidden";
+    }
+  }
+
+  _enableWindowScroll() {
+    if (this.els.scroll) {
+      this.els.scroll.style.overscrollBehavior = "";
+      this.els.scroll.style.overscrollBehaviorY = "";
+      this.els.scroll.style.overflowX = "";
+      this.els.scroll.style.overflowY = "";
+    }
+  }
+
+  _disableTransitions() {
+    if (this.els.transition) {
+      this.els.transition.style.transition = "none";
+    }
+  }
+
+  _enableTransitions() {
+    if (this.els.transition) {
+      this.els.transition.style.transition = "";
+    }
+  }
+  
+}
---
+++ src/PanZoom.js
@@ -0,0 +1,203 @@
+"use strict";
+/**
+ * @file
+ * PanZoom.js
+ */
+
+class PanZoom {
+  constructor(el, config = {}) {
+    this.limits = {
+      zoom: { min: config.minZoom || .5, max: config.maxZoom || 5 },
+      x: { min: config.minX || 0, max: config.maxX || 100 },
+      y: { min: config.minY || 0, max: config.maxY || 100 },
+    };
+
+    this.state = {
+      initial: {
+        zoom: config.initialZoom || 1,
+        x: config.initialX,
+        y: config.initialY,
+      },
+      zoom: config.initialZoom || 1,
+      x: null,
+      y: null,
+    };
+
+    this.el = el;
+
+    // Initialize once we've loaded the first background image.
+    const me = this;
+    const img = new Image();
+    img.src = PanZoomUtil.getBgUrl(this.el);
+    img.onload = function() {
+      me.init(img.width, img.height);
+    };
+  }
+
+  init(imgWidth, imgHeight) {
+    // Image 1:1 size.
+    this.state.img = {
+      w: imgWidth,
+      h: imgHeight,
+    };
+
+    this._findPanLimits();
+
+    // If we didn't provide an initial position, set it to center/center.
+    if (isNaN(this.state.initial.x) || isNaN(this.state.initial.y)) {
+      this.state.initial.x = (this.limits.x.min - this.limits.x.max) / 2;
+      this.state.initial.y = (this.limits.y.min - this.limits.y.max) / 2;
+    }
+
+    this.reset();
+
+    // Add event listener for window resizing, because this will change our
+    // display limits. If we need to throttle, see:
+    // https://bencentra.com/code/2015/02/27/optimizing-window-resize.html
+    window.addEventListener("resize", this._findPanLimits.bind(this));
+  }
+
+  /**
+   * Reset our zoom and position to the initial state.
+   */
+  reset() {
+    this.state.zoom = this.state.initial.zoom;
+    this.state.x = this.state.initial.x;
+    this.state.y = this.state.initial.y;
+    return this.update();
+  }
+
+  /**
+   * Any time we initialize, or resize the window, recalculate our offset
+   * limits based on the new viewport display size.
+   */
+  _findPanLimits() {
+    this.state.viewport = {
+      w: this.el.offsetWidth,
+      h: this.el.offsetHeight,
+    };
+
+    this.limits.x.max = 0;
+    this.limits.x.min = this.state.viewport.w - (this.state.img.w * this.state.zoom);
+    this.limits.y.max = 0;
+    this.limits.y.min = this.state.viewport.h - (this.state.img.h * this.state.zoom);
+  }
+
+  zoomIn(focus = null) { return this.zoomTo(this.state.zoom * 1.5, focus); }
+  zoomOut(focus = null) { return this.zoomTo(this.state.zoom * .75, focus); }
+
+  zoomTo(zoomLevel, focus = null) {
+    if (this.state.zoom === this.limits.zoom.max && zoomLevel > this.state.zoom) {
+      // We're already at maximum zoom, and we're trying to zoom in further.
+      // Do nothing.
+      return this.state;
+    }
+
+    const state = this.state;
+
+    if (focus === null) {
+      focus = {
+        x: state.viewport.w / 2,
+        y: state.viewport.h / 2,
+      };
+    }
+
+    // Determine the location we're going to zoom into/out from, based on the
+    // given focus point within the viewport.
+    let percX = (Math.abs(state.x) + focus.x) / (state.img.w * state.zoom);
+    let percY = (Math.abs(state.y) + focus.y) / (state.img.h * state.zoom);
+
+    // Calculate the new left/top position for the background image.
+    this.state.x = focus.x - (percX * state.img.w * zoomLevel);
+    this.state.y = focus.y - (percY * state.img.h * zoomLevel); 
+    this.state.zoom = zoomLevel;
+
+    this._findPanLimits();
+    return this.update();
+  }
+
+  panLeft() { return this._pan("left"); }
+  panRight() { return this._pan("right"); }
+  panUp() { return this._pan("up"); }
+  panDown() { return this._pan("down"); }
+
+  panTo(position = {}) {
+    this.state.x = position.x;
+    this.state.y = position.y;
+    return this.update();
+  }
+  panOffset(position = {}) {
+    this.state.x += position.x;
+    this.state.y += position.y;
+    return this.update();
+  }
+
+  /**
+   * Pan an offset amount, but don't record this as our new position. This is
+   * what we use to implement a mouse drag behaviour to interactively pan the
+   * background image.
+   */
+  panOffsetLive(offset = {}) {
+    const limits = this.limits;
+
+    const newX = PanZoomUtil.clamp(this.state.x + offset.x, limits.x.min, limits.x.max);
+    const newY = PanZoomUtil.clamp(this.state.y + offset.y, limits.y.min, limits.y.max);
+
+    this.el.style.backgroundPosition = `${newX}px ${newY}px`;
+    return { x: newX, y: newY };
+  }
+
+  /**
+   * Partially pan the viewport in a given direction.
+   */
+  _pan(direction) {
+    const delta = .4; // Distance = 40% of the viewport direction.
+
+    switch (direction) {
+      case "left": this.state.x += this.state.viewport.w * delta; break;
+      case "right": this.state.x -= this.state.viewport.h * delta; break;
+      case "up": this.state.y += this.state.viewport.w * delta; break;
+      case "down": this.state.y -= this.state.viewport.h * delta; break;
+    }
+
+    return this.update();
+  }
+
+  /**
+   * Update the background image to the new scale and position.
+   *
+   * Values are checked and corrected against maximum/minimum values.
+   */
+  update() {
+    const limits = this.limits;
+
+    [ "zoom", "x", "y" ].forEach(key => {
+      this.state[key] = PanZoomUtil.clamp(this.state[key], limits[key].min, limits[key].max);
+    });
+
+    this.el.style.backgroundSize = `${this.state.img.w * this.state.zoom}px`;
+    this.el.style.backgroundPosition = `${this.state.x}px ${this.state.y}px`;
+
+    return this.state;
+  }
+
+}
+
+class PanZoomUtil {
+  constructor() {}
+
+  static clamp(num, min, max) {
+    return Math.min(Math.max(num, min), max);
+  }
+
+  /**
+   * Get the URL from a CSS background-image rule on an element.
+   */
+  static getBgUrl(el) {
+    return window
+      .getComputedStyle(el)
+      .backgroundImage
+      .replace(/"/g, "")
+      .replace(/url\(|\)$/ig, "");
+  }
+}
